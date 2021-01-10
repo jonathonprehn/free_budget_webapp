dd748227143dfb028541bbbfc26f57b90c8c0dde
Jonathon Prehn <horsentp@gmail.com>
2021-01-10 14:45:45 -0500

    Added Bootstrap navbar

--- html/manageCategories.php
+++ html/manageCategories.php
@@ -7,7 +7,10 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 <body>
-
+<?php
+	require('common_functions.php');
+	gen_nav_bar();
+?>
 <div class="container">
 	<div class="row">
 		<div class="col-12">
